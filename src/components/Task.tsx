2d8a728ecfcff55d7808ce6c7e5acdbc1e04f56c
BrunaBudel <brunakbudel@gmail.com>
2023-07-28 15:42:08 -0300

    added thhe logo and deleted some imports

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react';
 import { ITask } from './List';
 
 export interface TaskProps extends ITask {
